c62e7f029975464116e05ad0bae0a40d2d2b1540
Dorian Pavetić <70510806+dorianpavetic@users.noreply.github.com>
2023-09-02 17:05:56 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd functions; npm ci
 
-if [ -z "${FIREBASE_TOKEN}" ]; then
+if [ -z "${GCP_SA_KEY}" ]; then
     echo "FIREBASE_TOKEN is missing"
     exit 1
 fi
@@ -10,6 +10,19 @@ fi
 if [ -z "${TARGET}" ]; then
     echo "TARGET is missing - setting 'default'"
     TARGET = "default"
+fi
+
+if [ -n "$GCP_SA_KEY" ]; then
+  if echo "$GCP_SA_KEY" | jq empty 2>/dev/null; then
+    echo "Storing GCP_SA_KEY in /opt/gcp_key.json"
+    echo "$GCP_SA_KEY" > /opt/gcp_key.json
+  else
+    echo "Storing the decoded GCP_SA_KEY in /opt/gcp_key.json"
+    echo "$GCP_SA_KEY" | base64 -d > /opt/gcp_key.json # If encoded base64 key, decode and save
+  fi
+
+  echo "Exporting GOOGLE_APPLICATION_CREDENTIALS=/opt/gcp_key.json"
+  export GOOGLE_APPLICATION_CREDENTIALS=/opt/gcp_key.json
 fi
 
 npx firebase use ${TARGET}
